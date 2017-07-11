54e821976d6ea40ca364fa0d216303662add5e4b
Mumshad Mannambeth <mmumshad@gmail.com>
2017-07-11 13:38:20 -0400

    Enable seed db for production based on environment variable

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -20,5 +20,8 @@ module.exports = {
       || process.env.MONGOHQ_URL
       || process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME
       || 'mongodb://localhost/app2'
-  }
+  },
+
+  // Seed database on startup
+  seedDB: process.env.SEED_DB || false
 };
