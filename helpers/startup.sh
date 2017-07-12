3e90928e356e1decaf97ad887a2adc43b23ea8ea
Mumshad Mannambeth <mmumshad@gmail.com>
2017-07-12 09:53:45 -0400

    Add messages

--- helpers/startup.sh
+++ helpers/startup.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 COLOR_GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 
-echo ""
+echo -e "${COLOR_GREEN}-----------------------------------------------------"
+echo -e "              Start SSH Service"
+echo -e "-----------------------------------------------------${NC}"
 
 # Start the ssh service. This is required by the playable web server
 service ssh start
@@ -11,12 +14,20 @@ service ssh start
 
 if [[ ${MOUNT_S3} = "True" ]];
   then
-    echo "Mounting Amazon S3 Bucket to /opt/ansible-projects"
+    echo -e "${COLOR_GREEN}-----------------------------------------------------"
+    echo "       Mounting Amazon S3 Bucket to /opt/ansible-projects"
+    echo -e "-----------------------------------------------------${NC}"
     yas3fs s3://${S3_PATH} /opt/ansible-projects -f
 fi
 
+echo -e "${COLOR_GREEN}-----------------------------------------------------"
+echo -e "              Start MONGODB Service"
+echo -e "-----------------------------------------------------${NC}"
 # Start MongoDB database in the background
 mongod &
 
+echo -e "${COLOR_GREEN}-----------------------------------------------------"
+echo -e "              Start Web Server"
+echo -e "-----------------------------------------------------${NC}"
 # Serve web server
 gulp serve:dist:no_build
